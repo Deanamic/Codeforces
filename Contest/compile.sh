698d26d534bf0c76fd50a1a3eb15b6fd91acc3f2
Dean <deanzhu1@gmail.com>
2017-04-06 22:00:59 +0200

    Update compile.sh

--- Contest/compile.sh
+++ Contest/compile.sh
@@ -1,5 +1,5 @@
 num=${PWD##*/}
 for let in ${@}
 do
-    g++ ${let}${num}.cc -o bin/${let}.out 2> debug/${let}${num}.txt
+    g++ bin/${let}${num}.cc -o bin/${let}.out 2> debug/${let}${num}.txt
 done
