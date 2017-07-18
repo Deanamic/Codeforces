367afcdde41dc50314281258a26d2b8f30ed27d8
Dean Zhu <deanzhu1@gmail.com>
2017-07-19 00:22:08 +0200

    programmed problem B, waiting for codeforces servers

--- Virtual/CF613/B/BCF613.cc
+++ Virtual/CF613/B/BCF613.cc
@@ -4,5 +4,55 @@ using namespace std;
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	long long n,A,cf,cm,Q;
+	cin >> n >> A >> cf >> cm >> Q;
+	pair<long long,int> v[n+1];
+	for(int i = 0; i < n; ++i){
+		cin >> v[i].first;
+		v[i].second = i;
+	}
+	v[n].first = A;
+	sort(v, v+n);
+	long long pref[n+1];
+	pref[0] = 0;
+	for(int i = 1; i < n+1; ++i) pref[i] = pref[i-1] + v[i-1].first;
 
+	long long itmax = -1, itmin = -1;
+	long long curmax = n, curmin = n-1;
+	long long ansmax = -1;
+	long long qmax;
+	while(Q >= 0 and curmax >= 0) {
+		Q -= A - v[curmax].first;
+		curmin = min(curmin, curmax - 1);
+		if(Q >= 0) {
+			long long curans = (n-curmax) * cf;
+			// cerr << "curmax: " << curmax << endl;
+			//cerr << "curans: " << curans << endl;
+			// cout << "curmin: " << curmin << endl;
+			while(curmin >= 0) {
+				if((pref[curmin+1] + Q) / (curmin+1) >= v[curmin].first) break;
+				--curmin;
+			}
+			//cerr << "Q: " << Q << endl;
+			//cout << "curmin: " << curmin + 1<< endl;
+			//cerr << pref[curmin+1] << endl;
+			if(curmin >= 0) curans += min(A, v[curmin].first + (Q - (v[curmin].first * (curmin+1) - pref[curmin+1]))/(curmin+1)) * cm;
+			else curans += A * cm;
+			if(curans > ansmax) {
+				itmax = curmax;
+				itmin = curmin;
+				ansmax = curans;
+				qmax = Q;
+			}
+			//cerr << curans << endl;
+		}
+		--curmax;
+	}
+	cout << ansmax << endl;
+	int ans[n];
+	for(int i = 0; i < n; ++i) ans[v[i].second] = (i >= itmax ? A : (i <= itmin ? min(A, v[itmin].first + (qmax - (v[itmin].first * (itmin+1) - pref[itmin+1]))/(itmin+1)) : v[i].first));
+	for(int i = 0; i < n; ++i) {
+		cout << ans[i] << ' ';
+	}
+	cout << endl;
 }
--- Virtual/CF613/Readme.md
+++ Virtual/CF613/Readme.md
@@ -1,4 +1,4 @@
-07/17 CF 613 [round 339 Div1](http://codeforces.com/contest/613)
+07/17 [CF 613 round 339 Div1](http://codeforces.com/contest/613)
 
 - [x] Problem A  
 The problem reduces to the following:  
@@ -7,10 +7,19 @@ The furthest point must be a vertex whereas the nearest point can lie on a segme
 Still it can easily be found by a ternary search or simply by formulae.  
 Complexity O(nlogn) or O(n).
 
-- [ ] Problem B
+- [x] Problem B  
+We are given an array of N <= 1e5 numbers, a<sub>i</sub> <= A. Now we can do Q operations of augmenting each point in the array by 1.  
+We will say the value of the array is |a<sub>i</sub> = A| * c<sub>f</sub> + the minimum * c<sub>m</sub>.  
+Given N, A, Q, c<sub>f</sub>, c<sub>m</sub>, what is the maximum value of the array.  
+We can clearly see that all our operations will be used to make more numbers equal to A, or make the maximum of the array higher.  
+If we want to raise numbers to the maximum it's obvious to use the greedy strategy of adding to the upper part, now if we have Q' operations left
+we want to find the maximum possible minimum.  
+Lets first sort the array an iterate over the number of values to max out, for each value we will have a maximum achievable minimum, as this value is non-increasing we can simply
+use a second pointer to update the minimum.  
+Complexity O(nlogn) for the sort.
 
-- [ ] Problem C
+- [ ] Problem C  
 
-- [ ] Problem D
+- [ ] Problem D  
 
-- [ ] Problem E
+- [ ] Problem E  
