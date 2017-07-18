e80c10bd4df82e99c4afc7bff33c97bf1f300a88
Dean Zhu <deanzhu1@gmail.com>
2017-07-18 20:15:38 +0200

    modified contest scripts to a simpler structure added hightail (credits to dj3500) for contest parsing

--- Contest/createfile.sh
+++ Contest/createfile.sh
@@ -2,6 +2,7 @@ num=${PWD##*/}
 for let in {A..Z}
 do
   mkdir ${let}
+  touch Readme.md
   touch ${let}/${let}${num}.cc
   touch ${let}/Debug.txt
   printf "#include<bits/stdc++.h>\nusing namespace std;\n\nint main(){\n\tios::sync_with_stdio(0);\n\tcin.tie(0);\n\n}\n" >> ${let}/${let}${num}.cc
