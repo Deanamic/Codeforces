e0e8cb46b166c22ca2a410ab20d9925c1688796c
Dean <deanzhu1@gmail.com>
2017-07-19 12:29:18 +0200

    Update BCF613.cc

--- Virtual/CF613/B/BCF613.cc
+++ Virtual/CF613/B/BCF613.cc
@@ -20,37 +20,31 @@ int main(){
 	long long itmax = -1, itmin = -1;
 	long long curmax = n, curmin = n-1;
 	long long ansmax = -1;
-	long long qmax;
+	int minval;
 	while(Q >= 0 and curmax >= 0) {
 		Q -= A - v[curmax].first;
 		curmin = min(curmin, curmax - 1);
 		if(Q >= 0) {
 			long long curans = (n-curmax) * cf;
-			// cerr << "curmax: " << curmax << endl;
-			//cerr << "curans: " << curans << endl;
-			// cout << "curmin: " << curmin << endl;
 			while(curmin >= 0) {
 				if((pref[curmin+1] + Q) / (curmin+1) >= v[curmin].first) break;
 				--curmin;
 			}
-			//cerr << "Q: " << Q << endl;
-			//cout << "curmin: " << curmin + 1<< endl;
-			//cerr << pref[curmin+1] << endl;
 			if(curmin >= 0) curans += min(A, v[curmin].first + (Q - (v[curmin].first * (curmin+1) - pref[curmin+1]))/(curmin+1)) * cm;
 			else curans += A * cm;
 			if(curans > ansmax) {
 				itmax = curmax;
 				itmin = curmin;
 				ansmax = curans;
-				qmax = Q;
+				if(curmin >= 0)
+				  minval = min(A, v[curmin].first + (Q - (v[curmin].first * (curmin+1) - pref[curmin+1]))/(curmin+1));
 			}
-			//cerr << curans << endl;
 		}
 		--curmax;
 	}
 	cout << ansmax << endl;
 	int ans[n];
-	for(int i = 0; i < n; ++i) ans[v[i].second] = (i >= itmax ? A : (i <= itmin ? min(A, v[itmin].first + (qmax - (v[itmin].first * (itmin+1) - pref[itmin+1]))/(itmin+1)) : v[i].first));
+	for(int i = 0; i < n; ++i) ans[v[i].second] = (i >= itmax ? A : (i <= itmin ? minval : v[i].first));
 	for(int i = 0; i < n; ++i) {
 		cout << ans[i] << ' ';
 	}
