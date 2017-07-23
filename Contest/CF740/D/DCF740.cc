4255ef5b2780322a468b552276e87fddf0aedeb3
Dean Zhu <deanzhu1@gmail.com>
2017-07-23 13:30:59 +0200

    AC problem 740D

--- Contest/CF740/D/DCF740.cc
+++ Contest/CF740/D/DCF740.cc
@@ -1,8 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int MAXN = 200010;
+
+int a[MAXN];
+long long d[MAXN];
+int p[20][MAXN];
+vector<pair<int,int>> adj[MAXN];
+int pref[MAXN] = {};
+
+void depth(int x, long long dth) {
+	d[x] = dth;
+	for(pair<int,int> y : adj[x]) depth(y.first, y.second + dth);
+}
+
+void findnode(int x, long long dth) {
+	int it = 0;
+	while(it < 20 and p[it][x] != -1 and d[p[it][x]] >= dth) ++it;
+	if(it == 0) {
+		if(p[0][x] >= 0) pref[p[0][x]]--;
+		return;
+	}
+	return findnode(p[it-1][x], dth);
+}
+
+int getans(int x) {
+	for(pair<int,int> y : adj[x]) pref[x] += getans(y.first);
+	return pref[x];
+}
+
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	int n, m;
+	cin >> n;
+	for(int i = 0; i < n; ++i) cin >> a[i];
+	for(int i = 1; i < n; ++i){
+		int x,y;
+		cin >> x >> y;
+		--x;
+		adj[x].push_back({i, y});
+		p[0][i] = x;
+	}
+	depth(0, 0);
+	for(int i = 0; i < 20; ++i){
+		p[i][0] = -1;
+	}
+	for(int i = 1; i < 20; ++i) {
+		for(int j = 1; j < n; ++j){
+			if(p[i-1][j] >= 0) p[i][j] = p[i-1][p[i-1][j]];
+			else p[i][j] = -1;
+		}
+	}
 
+	for(int i = 0; i < n; ++i) {
+		if(p[0][i] != -1) pref[p[0][i]]++;
+
+		findnode(i, d[i] - a[i]);
+
+	}
+
+	getans(0);
+
+	for(int i = 0; i < n; ++i) cout << pref[i] << '\n';
 }
--- Contest/CF740/Readme.md
+++ Contest/CF740/Readme.md
@@ -9,7 +9,12 @@ Codeforces 740 Round 381 Div 2
 - [x] Problem B  
 
 - [x] Problem C  
+We can first see that the answer is at most the minimum length of a segment + 1. We can see that if the array values are:  
+0, 1, 2, 3, .., ans - 1, 0 ,.. no matter what segment of length ans we pick we will always find that the mex is ans.  
 
-- [ ] Problem D  
+- [x] Problem D  
+So playing around the inequality we can see that v controls u iff depth[v] >= depth[u] - a[u].  
+Also as the depth[v] is a monotonous function which only grows as we go down the tree there exist one node such that all its child and itself fulfil the inequality. We can binary lift for this answer and obtain values using prefix sums.  
+Complexity: O(nlogn)
 
 - [ ] Problem E  
