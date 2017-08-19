1b7516d828c24a94220e9fb9c7e4b88680247d79
Dean Zhu <deanzhu1@gmail.com>
2017-08-19 16:32:48 +0200

    Updated Readme

--- Contest/createfile.sh
+++ Contest/createfile.sh
@@ -1,12 +1,21 @@
 num=${PWD##*/}
 
 touch Readme.md
+printf "Codeforces XXX Round YYY Div Z\n" >> Readme.md
+printf "[Contest Link](http://codeforces.com/contest/XXX)\n" >> Readme.md
+printf "[Editorial Link](http://codeforces.com/blog/entry/)\n\n" >> Readme.md
+
+printf "Atcoder XXX Contest YYY\n" >> Readme.md
+printf "[Contest Link](http://XXXYYY.contest.atcoder.jp/)\n" >> Readme.md
+printf "[Editorial Link](https://atcoder.jp/img/XXXYYY/editorial.pdf)\n\n" >> Readme.md
+
 for let in {A..Z}
 do
   mkdir ${let}
   touch ${let}/${let}${num}.cc
   touch ${let}/Debug.txt
   echo - [ ] Problem ${let} >> Readme.md
+  printf "\n" >> Readme.md
   printf "#include<bits/stdc++.h>\nusing namespace std;\n\nint main(){\n\tios::sync_with_stdio(0);\n\tcin.tie(0);\n\n}\n" >> ${let}/${let}${num}.cc
   if [[ ${let} = ${1} ]]; then
     exit 1
